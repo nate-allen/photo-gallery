8854b5d0aa6bf27436b534b8183ce45516eb6ea1
Nathan Allen <nathan@fireflypartners.com>
2016-02-02 14:04:06 -0500

    Created a main admin navigation file

---
+++ admin/includes/navigation.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">SB Admin</a>
+    </div>
+
+    <?php include("navigation_top.php") ?>
+
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <?php include("navigation_side.php") ?>
+    </div>
+</nav>
