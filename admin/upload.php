008d3def25a789d8805e7eb38f5a9ece90d54bb5
Nathan Allen <nathan@fireflypartners.com>
2016-02-03 09:32:32 -0500

    Setup temporary page for future use

---
+++ admin/upload.php
@@ -0,0 +1,30 @@
+<?php include("includes/header.php"); ?>
+
+        <?php include("includes/navigation.php") ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Upload
+                            <small>Subheading</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Blank Page
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+  <?php include("includes/footer.php"); ?>
