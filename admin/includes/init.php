1d3147724da24b0361de01152fc4c4e1dbaad45d
Nathan Allen <nathan@fireflypartners.com>
2016-02-03 14:46:39 -0500

    Added an autoloader instead of requiring classes manually

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,5 +1,13 @@
 <?php
+// TODO: Make a better autoloader once we start using namespaces...
+function photo_gallery_autoloader($class) {
+    $class = strtolower( $class );
+    $the_path = "includes/{$class}.php";
 
-include("configuration.php");
-include("database.php");
-include("user.php");
+    if ( file_exists($the_path)) {
+        require($the_path);
+    } else {
+        die("This file name {$class}.php was not found.");
+    }
+}
+spl_autoload_register('photo_gallery_autoloader');
