9a3730dc305a062f0c5885d1a2e0523420956177
Nathan Allen <nathan@fireflypartners.com>
2016-02-09 10:29:39 -0500

    Created login form

---
+++ admin/login.php
@@ -0,0 +1,54 @@
+<?php require_once("includes/header.php") ?>
+<?php
+
+if ( $session->is_signed_in() ) {
+    $session->redirect("index.php");
+}
+
+if ( isset($_POST['submit']) ) {
+
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    $user_found = User::verify_user( $username, $password );
+
+    if ($user_found) {
+        $session->login($user_found);
+        $session->redirect("index.php");
+    } else {
+        $message = "Your username or password was incorrect.";
+    }
+
+} else {
+
+    $username = "";
+    $password = "";
+
+}
+
+?>
+<div class="col-md-4 col-md-offset-3">
+
+    <?php if (isset($message)) : ?>
+        <h4 class="bg-danger"><?php echo $message; ?></h4>
+    <?php endif; ?>
+
+    <form id="login-id" action="" method="post">
+
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
+        </div>
+
+        <div class="form-group">
+            <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+        </div>
+
+    </form>
+
+</div>
